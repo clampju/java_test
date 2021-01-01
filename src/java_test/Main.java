3e7c4b5e080b57be50495d07d59b2eda87b5b976
clampju <clampju@126.com>
2021-01-01 15:53:07 +0800

    tortoiseGit第一次提交

--- src/java_test/Main.java
+++ src/java_test/Main.java
@@ -3,7 +3,7 @@ package java_test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello,world!");
+		System.out.println("hello,world!xxxx");
 		System.out.println("sdfgdfhd,worlxxxd!");
 	}
 
