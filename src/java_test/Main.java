09745cd8d7b5a8667493d3119ffb5e5bac631736
clampju <clampju@126.com>
2021-01-03 16:01:40 +0800

    idea 第一次提交

--- src/java_test/Main.java
+++ src/java_test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello,world!xxxx");
 		System.out.println("sdfgdfhd,worlxxxd!");
+		System.out.println("idea push");
 	}
 
 }
